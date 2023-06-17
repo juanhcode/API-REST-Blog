41916716fdf933b2287f4d022a1b1ca617570c94
Juan Hoyos <juanhoyos1347@gmail.com>
2023-06-17 02:36:04 -0500

    structure

---
+++ src/main/java/com/apirestblog/apirestblog/ApiRestBlogApplication.java
@@ -0,0 +1,13 @@
+package com.apirestblog.apirestblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRestBlogApplication.class, args);
+    }
+
+}
